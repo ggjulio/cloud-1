23d810e36017492f0103111a3ac073000605703e
dunk4n <nicolasduchesne884@gmail.com>
2022-07-22 22:37:15 +0200

    roles set

--- startup.sh
+++ startup.sh
@@ -7,16 +7,12 @@ sudo apt install ansible
 # Install Ansible modules
 ansible-galaxy collection install -r requirements.yaml
 
-# Readme help
-tput init; tput setaf 41; echo "Add ansible env variable for no host key checking:"
-tput setaf 250; echo "  \`export ANSIBLE_HOST_KEY_CHECKING=False\`"; echo
-tput setaf 41; echo "Set DB and Wordpress password:"
-tput setaf 250; echo "  \`export MYSQL_ROOT_PASSWORD=plussimple; export MYSQL_USER=wordpress; export MYSQL_PASSWORD=password; export MYSQL_DATABASE=wordpress\`"; echo
-tput setaf 41; echo "Execute Ansible playbook:"
-tput setaf 250; echo "  \`ansible-playbook -i hosts cloud-1.yaml\`"
-
 #example
 # REMOTE_USER=badass ansible-playbook install-docker.yaml -K
 
 #create a brige to the host to use localhost to access to phpmyadmin
 #ssh -N -L 3350:127.0.0.1:8080 -i ~/.ssh/id_rsa badass@127.0.0.1
+
+# run ansible
+# MYSQL_DATABASE doit etre a 'exampledb' pour marche il doit y avoir un fichier set quelque part ou il faut change exampledb en template
+# REMOTE_USER=root MYSQL_USER=exampleuser MYSQL_PASSWORD=examplepass MYSQL_DATABASE=exampledb ansible-playbook cloud-1.yaml
