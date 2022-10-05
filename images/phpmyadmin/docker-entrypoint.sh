5bceda17fac88fd36a2a45c6b4bf9cb93130b353
ggjulio <juligonz@student.42.fr>
2022-10-05 21:57:48 +0200

    pma working fine

--- images/phpmyadmin/docker-entrypoint.sh
+++ images/phpmyadmin/docker-entrypoint.sh
@@ -6,9 +6,9 @@ if [ "$(ls -A /var/www/app)" ]; then
 	echo "phpmyadmin already downloaded"
 else
 	echo "Persistent volume empty, downloading phpmyadmin..."
-	wget -O /var/www/app/adminer.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql.php
-	# wget -q -O - https://files.phpmyadmin.net/phpMyAdmin/5.2.0/phpMyAdmin-5.2.0-english.tar.gz \
-	# 	| tar -xz -C /var/www/app --strip-components=1
+	wget -q -O - https://files.phpmyadmin.net/phpMyAdmin/5.2.0/phpMyAdmin-5.2.0-english.tar.gz \
+		| tar -xz -C /var/www/app --strip-components=1
+	cp -v /tmp/config.inc.php /var/www/app/
 	echo "Done."
 fi
 
