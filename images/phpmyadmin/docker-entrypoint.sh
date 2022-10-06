77b939733791110122f2cec18addbfc8cf64c2d9
ggjulio <juligonz@student.42.fr>
2022-10-06 16:53:02 +0200

    fixed 2 warnings in phpmyadmin

--- images/phpmyadmin/docker-entrypoint.sh
+++ images/phpmyadmin/docker-entrypoint.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
-set -e
+set -eu
 
-if [ "$(ls -A /var/www/app)" ]; then
+APP_PATH='/var/www/app/'
+CONFIG_FILENAME="${APP_PATH}config.inc.php"
+
+if [ "$(ls -A $APP_PATH)" ]; then
 	echo "phpmyadmin already downloaded"
 else
 	echo "Persistent volume empty, downloading phpmyadmin..."
 	wget -q -O - https://files.phpmyadmin.net/phpMyAdmin/5.2.0/phpMyAdmin-5.2.0-english.tar.gz \
-		| tar -xz -C /var/www/app --strip-components=1
-	cp -v /tmp/config.inc.php /var/www/app/
+		| tar -xz -C $APP_PATH --strip-components=1
+	cp -v /tmp/config.inc.php $APP_PATH \
+		&& rm /tmp/config.inc.php
+	random_blowfish_secret="$(openssl rand -hex 16)"
+	sed -i "/'blowfish_secret'/c\\\$cfg['blowfish_secret'] = '$random_blowfish_secret';" $CONFIG_FILENAME
 	echo "Done."
 fi
 
